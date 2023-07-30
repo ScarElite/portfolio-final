0849c0ff0e2bfc19e34d19459d21bbae1131af41
Mitchell Armstrong <mra0211@gmail.com>
2023-07-30 18:00:21 -0400

    Created the Main section

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 const Main = () => {
+    const handleButtonClick = () => {
+        console.log("Projects Button Clicked!")
+    }
+
     return (
-        <section id="Main" className="flex">
-            <h1 className="main-name">Hello, I'm Mitchell Armstrong</h1>
+        <section id="Main" className="flex flex-col justify-around">
+
+            <div className="main-name-div">
+                <h1 className="main-name text-center">HELLO,</h1>
+                <h1 className="main-name text-center">I'M MITCHELL ARMSTRONG</h1>
+            </div>
+            
+            <div className="main-statement-div">
+                <h2 className="main-statement text-center">Resourceful full stack developer specializing in creating dynamic, responsive web applications from ideation to deployment</h2>
+            </div>
+
+            <div className="main-button-div flex justify-center">
+                <button className="main-button text-center" onClick={handleButtonClick}>PROJECTS</button>
+            </div>
+
         </section>
     )
 }
